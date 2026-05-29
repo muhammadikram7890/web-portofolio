f4d2203ae8ccab991bcdb3f80b43fcba8afbd322
bebek_garut <ikdossantoscs@gmail.com>
2026-05-29 22:26:25 +0700

    Feat : Navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -6,22 +6,24 @@ import Link from "next/link";
 export default function Navbar() {
   const { t, locale } = useTranslation();
     return(
-        <div className="flex justify-between items-center">
-            <div className="">
-            <ul className="flex space-x-8 px-8">
-               <li>{t.home}</li>
-                <li>{t.about}</li>
-                <li>{t.projects}</li>
-                <li>{t.skills}</li>
-                <li>{t.contact}</li>
-            </ul>
-            </div>
-              <div className="">
-                <div className="flex space-x-2 px-8">
-                <Link href="/id" className={locale === "id" ? "font-bold" : ""}>ID</Link>
-                <Link href="/en" className={locale === "en" ? "font-bold ml-4" : ""}>EN</Link>
-                <Sun></Sun>
-                <Moon></Moon>
+        <div className="fixed w-full">
+            <div className="flex justify-between items-center mt-4">
+                <div className="">
+                <ul className="flex space-x-8 px-8">
+                <li>{t.home}</li>
+                    <li>{t.about}</li>
+                    <li>{t.projects}</li>
+                    <li>{t.skills}</li>
+                    <li>{t.contact}</li>
+                </ul>
+                </div>
+                <div className="">
+                    <div className="flex space-x-2 px-8">
+                    <Link href="/id" className={locale === "id" ? "font-bold" : ""}>ID</Link>
+                    <Link href="/en" className={locale === "en" ? "font-bold ml-4" : ""}>EN</Link>
+                    <Sun></Sun>
+                    <Moon></Moon>
+                    </div>
                 </div>
             </div>
         </div>
