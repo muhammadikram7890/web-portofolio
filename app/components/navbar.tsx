4a8382438a65688704ae5c0275d4ad958a0a7087
bebek_garut <ikdossantoscs@gmail.com>
2026-06-01 23:24:19 +0700

    Fix : Navbar padding

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -7,9 +7,9 @@ export default function Navbar() {
   const { t, locale } = useTranslation();
     return(
         <div className="fixed w-full">
-            <div className="flex justify-between items-center mt-4">
+            <div className="flex justify-between items-center mt-4 px-6">
                 <div className="">
-                <ul className="flex space-x-8 px-8">
+                <ul className="flex space-x-8">
                 <li>{t.home}</li>
                     <li>{t.about}</li>
                     <li>{t.projects}</li>
@@ -17,14 +17,12 @@ export default function Navbar() {
                     <li>{t.contact}</li>
                 </ul>
                 </div>
-                <div className="">
-                    <div className="flex space-x-2 px-8">
+                    <div className="flex space-x-2">
                     <Link href="/id" className={locale === "id" ? "font-bold" : ""}>ID</Link>
                     <Link href="/en" className={locale === "en" ? "font-bold ml-4" : ""}>EN</Link>
                     <Sun></Sun>
                     <Moon></Moon>
                     </div>
-                </div>
             </div>
         </div>
     )
