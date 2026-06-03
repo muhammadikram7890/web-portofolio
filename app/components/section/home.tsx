b679deedcef37cdeefe7b6cf6cd360e5c5585a44
bebek_garut <ikdossantoscs@gmail.com>
2026-06-03 23:33:56 +0700

    Feat : Home

--- app/components/section/home.tsx
+++ app/components/section/home.tsx
@@ -1,10 +1,20 @@
 "use client"
 import { useTranslation } from "@/context/translation-context"
+import Image from "next/image"
 
 export default function Home (){
     const {t} = useTranslation()
 
-    return <div>
-        <p className="text-shadow-amber-300">{t.description}</p>
+    return <div className="w-full min-h-screen flex justify-center items-center px-6">
+        <div className="flex justify-between w-full items-center space-x-2">
+            <div className="flex justify-center items-center ">
+              <p className="text-blue-900 text-2xl text-center">Muhammad Ikram</p>
+            </div>
+            <div>
+               
+                <p className="text-shadow-amber-300">{t.description}</p>
+                <Image src={"/"} alt="" width={25} height={25} />
+            </div>
+        </div>
     </div>
 }
