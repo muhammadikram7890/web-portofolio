eb35bcc1208e1581d89c8177878639cb5faf970a
bebek_garut <ikdossantoscs@gmail.com>
2026-06-05 23:21:06 +0700

    Feat : Font Navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -2,20 +2,27 @@
 import { Sun, Moon } from "lucide-react";
 import { useTranslation } from "@/context/translation-context";
 import Link from "next/link";
+import {Arimo} from "next/font/google"
+
+const arimo = Arimo({
+    subsets:['latin'],
+    weight:['400']
+})
+
 
 export default function Navbar() {
   const { t, locale } = useTranslation();
     return(
         <div className="fixed w-full">
             <div className="flex justify-between items-center mt-4 px-6">
-                <div className="">
-                <ul className="flex space-x-8">
-                <li>{t.home}</li>
-                    <li>{t.about}</li>
-                    <li>{t.projects}</li>
-                    <li>{t.skills}</li>
-                    <li>{t.contact}</li>
-                </ul>
+                <div >
+                    <ul className="flex space-x-8">
+                        <li className={`${arimo.className}`}>{t.home}</li>
+                        <li>{t.about}</li>
+                        <li>{t.projects}</li>
+                        <li>{t.skills}</li>
+                        <li>{t.contact}</li>
+                    </ul>
                 </div>
                     <div className="flex space-x-2">
                     <Link href="/id" className={locale === "id" ? "font-bold" : ""}>ID</Link>
