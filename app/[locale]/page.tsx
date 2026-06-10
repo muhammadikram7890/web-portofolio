0d0a84cbdbac0ae7a883919a0a20b4ed4388d662
bebek_garut <ikdossantoscs@gmail.com>
2026-06-10 23:59:52 +0700

    Feat : components

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,11 +1,17 @@
 import Navbar from "../components/navbar";
 import Home from "../components/section/home";
+import Project from "../components/section/project";
+import About from "../components/section/about";
+import Skill from "../components/section/skill";
 
 export default function Page() {
   return (
-    <div className="p-5">
+    <div className="px-2">
       <Navbar />
       <Home />
+      <About/>
+      <Project/>
+      <Skill/>
     </div>
   );
 }
